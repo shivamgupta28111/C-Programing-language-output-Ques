05c4265fc07b72704a184346f1d8a256a4dd92e9
Tube light Coder <70341160+shivamgupta28111@users.noreply.github.com>
2021-05-20 20:28:35 +0530

    Update questions.c

--- questions.c
+++ questions.c
@@ -1,3 +1,6 @@
+//                                                         ZOHO C output questions
+
+
 #include <stdio.h>
 
 void main()
@@ -15,6 +18,42 @@ void main()
 //mmmm
 //aaaa
 //nnnn
+
+#include <stdio.h>
+
+void main()
+{
+    char *p="Phone";
+    p++;
+    char c=(*p);
+    ++c;
+    p--;
+    printf("%c %s",c,p++);
+    
+}
+
+//output: 
+
+
+void main()
+{
+    int c[]={2,3,4,6,5};
+    
+    int j,*p=c,*q=c;
+    
+    for(j=0;j<5;j++){
+        printf("%d",*c);
+        ++q;}
+        
+        for(j=0;j<5;j++){
+            print("%d,*p");
+            ++p;
+        }
+    }
+}
+
+//output
+
 
 
 
